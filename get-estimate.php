6433bd401123e30f78201b49e0473b5995a318ac
KhalidHussanMajoka <108335078+KhalidHussanMajoka@users.noreply.github.com>
2023-01-09 15:56:01 +0500

    color changed

--- get-estimate.php
+++ get-estimate.php
@@ -72,7 +72,7 @@
                 <div class='avia_textblock  ' itemprop="text">
                   <p><strong>Wallace Ave The Bronx, NY 10462, USA</strong></p>
                   <p>NY 10462, USA</p>
-                  <p><strong><a href="mailto:enstimating@gmail.com">enstimating@gmail.com</a></strong></p>
+                  <p><strong><a href="mailto:info@allstateestimation.com">info@allstateestimation.com</a></strong></p>
                   <p>+1 (917) 983-4975</p>
                   <p>We have mentioned our phone number and mail address as well, on our site . We are just one click away from you. Contact us for any construction estimation service, construction takeoffs and other such mentioned services at our website.</p>
                   <p>Further, you can write to us by uploading your plan or any estimation service you want. So, save your precious money and catch us.</p>
